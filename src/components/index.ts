aa41242e8d1f12fbfbbf8ef3653320d43320b531
kodster28 <kody@cloudflare.com>
2025-08-07 15:59:20 -0500

    Remove productsbytag

--- src/components/index.ts
+++ src/components/index.ts
@@ -46,7 +46,6 @@ export { default as PlanInfo } from "./PlanInfo.astro";
 export { default as ProductChangelog } from "./ProductChangelog.astro";
 export { default as ProductReleaseNotes } from "./ProductReleaseNotes.astro";
 export { default as ProductFeatures } from "./ProductFeatures.astro";
-export { default as ProductsByTag } from "./ProductsByTag.astro";
 export { default as PublicStats } from "./PublicStats.astro";
 export { default as RelatedProduct } from "./RelatedProduct.astro";
 export { default as Render } from "./Render.astro";
